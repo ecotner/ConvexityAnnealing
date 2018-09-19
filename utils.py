7dc11dec49089fce8f090dfe26be0a5973063e08
ecotner-desktop <2.71828cotner@gmail.com>
2018-09-19 09:47:13 -0700

    added precision, recall and fbeta metrics

--- utils.py
+++ utils.py
@@ -24,3 +24,43 @@ def set_global_seed(seed, use_parallelism=True):
         session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
         sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
         K.set_session(sess)
+
+def precision_macro(y_true, y_pred, n_classes=10):
+    """ Calculates macro precision metric """
+    y_pred = K.argmax(y_pred, axis=-1)
+    y_true = K.argmax(y_true, axis=-1)
+    val = []
+    for label in range(n_classes):
+        y = K.equal(y_true, label)
+        yhat = K.equal(y_pred, label)
+        TP = K.sum(K.cast(K.equal(K.equal(yhat, True), K.equal(y, True)), dtype=tf.int32))
+        FP = K.sum(K.cast(K.equal(K.equal(yhat, True), K.equal(y, False)), dtype=tf.int32))
+        val.append(TP/(TP+FP))
+    return tf.reduce_mean(val)
+
+def recall_macro(y_true, y_pred, n_classes=10):
+    """ Calculates macro precision metric """
+    y_pred = K.argmax(y_pred, axis=-1)
+    y_true = K.argmax(y_true, axis=-1)
+    val = []
+    for label in range(n_classes):
+        y = K.equal(y_true, label)
+        yhat = K.equal(y_pred, label)
+        TP = K.sum(K.cast(K.equal(K.equal(yhat, True), K.equal(y, True)), dtype=tf.int32))
+        FN = K.sum(K.cast(K.equal(K.equal(yhat, False), K.equal(y, True)), dtype=tf.int32))
+        val.append(TP/(TP+FN))
+    return tf.reduce_mean(val)
+
+def fbeta_macro(y_true, y_pred, beta=1, n_classes=10):
+    """ Calculates macro precision metric """
+    y_pred = K.argmax(y_pred, axis=-1)
+    y_true = K.argmax(y_true, axis=-1)
+    val = []
+    for label in range(n_classes):
+        y = K.equal(y_true, label)
+        yhat = K.equal(y_pred, label)
+        TP = K.sum(K.cast(K.equal(K.equal(yhat, True), K.equal(y, True)), dtype=tf.int32))
+        FN = K.sum(K.cast(K.equal(K.equal(yhat, False), K.equal(y, True)), dtype=tf.int32))
+        FP = K.sum(K.cast(K.equal(K.equal(yhat, True), K.equal(y, False)), dtype=tf.int32))
+        val.append((1+beta**2)*TP/((1+beta**2)*TP+(beta**2)*FN+FP))
+    return tf.reduce_mean(val)
