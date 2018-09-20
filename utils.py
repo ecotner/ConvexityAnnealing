6e3bbdc33f9fc788a20dd1049e75fbd02c3fd732
ecotner-desktop <2.71828cotner@gmail.com>
2018-09-19 17:06:41 -0700

    fixed issue with calculating true/false positives/negatives using TF logic

--- utils.py
+++ utils.py
@@ -33,23 +33,27 @@ def precision_macro(y_true, y_pred, n_classes=10):
     for label in range(n_classes):
         y = K.equal(y_true, label)
         yhat = K.equal(y_pred, label)
-        TP = K.sum(K.cast(K.equal(K.equal(yhat, True), K.equal(y, True)), dtype=tf.int32))
-        FP = K.sum(K.cast(K.equal(K.equal(yhat, True), K.equal(y, False)), dtype=tf.int32))
-        val.append(TP/(TP+FP))
+        TP = tf.reduce_sum(K.cast(tf.logical_and(y, yhat), dtype=tf.float32))
+        FP = tf.reduce_sum(tf.cast(tf.logical_and(tf.logical_not(y), yhat), dtype=tf.float32))
+        #TN = tf.reduce_sum(tf.cast(tf.logical_and(tf.logical_not(y), tf.logical_not(yhat)), dtype=tf.float32))
+        #FN = tf.reduce_sum(tf.cast(tf.logical_and(y, tf.logical_not(yhat)), dtype=tf.float32))
+        val.append(tf.div(TP,TP+FP+1.0e-9))
     return tf.reduce_mean(val)
 
 def recall_macro(y_true, y_pred, n_classes=10):
-    """ Calculates macro precision metric """
+    """ Calculates macro recall metric """
     y_pred = K.argmax(y_pred, axis=-1)
     y_true = K.argmax(y_true, axis=-1)
     val = []
     for label in range(n_classes):
         y = K.equal(y_true, label)
         yhat = K.equal(y_pred, label)
-        TP = K.sum(K.cast(K.equal(K.equal(yhat, True), K.equal(y, True)), dtype=tf.int32))
-        FN = K.sum(K.cast(K.equal(K.equal(yhat, False), K.equal(y, True)), dtype=tf.int32))
-        val.append(TP/(TP+FN))
-    return tf.reduce_mean(val)
+        TP = tf.reduce_sum(K.cast(tf.logical_and(y, yhat), dtype=tf.float32))
+        #FP = tf.reduce_sum(tf.cast(tf.logical_and(tf.logical_not(y), yhat), dtype=tf.float32))
+        #TN = tf.reduce_sum(tf.cast(tf.logical_and(tf.logical_not(y), tf.logical_not(yhat)), dtype=tf.float32))
+        FN = tf.reduce_sum(tf.cast(tf.logical_and(y, tf.logical_not(yhat)), dtype=tf.float32))
+        val.append(tf.div(TP, TP+FN+1.0e-9))
+    return val[1]
 
 def fbeta_macro(y_true, y_pred, beta=1, n_classes=10):
     """ Calculates macro precision metric """
@@ -59,8 +63,12 @@ def fbeta_macro(y_true, y_pred, beta=1, n_classes=10):
     for label in range(n_classes):
         y = K.equal(y_true, label)
         yhat = K.equal(y_pred, label)
-        TP = K.sum(K.cast(K.equal(K.equal(yhat, True), K.equal(y, True)), dtype=tf.int32))
-        FN = K.sum(K.cast(K.equal(K.equal(yhat, False), K.equal(y, True)), dtype=tf.int32))
-        FP = K.sum(K.cast(K.equal(K.equal(yhat, True), K.equal(y, False)), dtype=tf.int32))
-        val.append((1+beta**2)*TP/((1+beta**2)*TP+(beta**2)*FN+FP))
+        TP = tf.reduce_sum(K.cast(tf.logical_and(y, yhat), dtype=tf.float32))
+        FP = tf.reduce_sum(tf.cast(tf.logical_and(tf.logical_not(y), yhat), dtype=tf.float32))
+        #TN = tf.reduce_sum(tf.cast(tf.logical_and(tf.logical_not(y), tf.logical_not(yhat)), dtype=tf.float32))
+        FN = tf.reduce_sum(tf.cast(tf.logical_and(y, tf.logical_not(yhat)), dtype=tf.float32))
+        val.append(tf.div((1+beta**2)*TP, (1+beta**2)*TP+(beta**2)*FN+FP + 1.0e-9))
     return tf.reduce_mean(val)
+
+def test_metric_y_true(y_true, y_pred): print("y_true shape=", y_true); return K.shape(y_true)[1]
+def test_metric_y_pred(y_true, y_pred): print("y_pred shape=", y_pred); return K.shape(y_pred)[1]
