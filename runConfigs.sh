1ef5009223d775c2c82eab2fde098ed4a3f5f363
ecotner-desktop <2.71828cotner@gmail.com>
2018-10-04 10:07:26 -0700

    fixed a bunch of issues caused by __pycache__ folders preventing training from running properly

--- runConfigs.sh
+++ runConfigs.sh
@@ -6,20 +6,28 @@
 # necessary directories itself), then moves the config to the experiment's
 # directory and grabs the next one, repeating until ConfigFiles is empty.
 
+echo "Beginning training" > train.log
 # While ConfigFiles is not empty
 while [ -n "$(ls Experiments/ConfigQueue)" ]; do
 	# Move first available file to the working directory
 	fpath=$(echo Experiments/ConfigQueue/*.py)	# Gets all file paths
 	fpath=${fpath%% *}				# Gets first file path
 	fname=${fpath##*/}				# Gets file name
+	echo "fpath: ${fpath}" >> train.log
+	echo "fname: ${fname}" >> train.log
 	mv "$fpath" "config.py"				# Move to pwd
 	# Run train.py (this generates experiment directory)
+	echo "Training ${fname}" >> train.log
 	echo "Training ${fname}"
-	python3 train.py
+	expName=$(python3 -B -c "import config; print(config.Config().NAME)")	# Get experiment name from python file
+	expPath=$(python3 -B -c "import config; print(config.Config().SAVE_PATH)")	# Get experiment save path from python file
+	echo "expName: ${expName}" >> train.log
+	echo "expPath: ${expPath}" >> train.log
+	python3 -B train.py 2>> trainerrors.log
 	# Move config into generated directory
-	expName=$(python3 -c "import config; print(config.Config().NAME)")	# Get experiment name from python file
-	mv "config.py" "Experiments/${expName}/config_${expName}"
+	mv "config.py" "${expPath}config_${expName}.py"
+	echo "Done training ${expName}" >> train.log
 	echo "Done training ${expName}"
 done
-# Print "done"?
+echo "All experiments finished training" >> train.log
 echo "All experiments finished training"
